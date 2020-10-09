2642dfe9d7885d160bfde6b6ccaff04aee77ad02
Sandeep Gamot <sandygamot77@gmail.com>
2020-10-09 17:01:02 +0530

    fix:double vote check on client side & added landing page

--- src/utils/getHoursRemaining.js
+++ src/utils/getHoursRemaining.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default (timestamp) => {
   let hours = Math.round((timestamp - Date.now()) / (60 * 60 * 1000));
 
@@ -14,20 +13,3 @@ export default (timestamp) => {
 
   return data;
 };
-=======
-export default (timestamp) => {
-  let hours = Math.round((timestamp - Date.now()) / (60 * 60 * 1000));
-
-  let data = { timeLeft: `${hours}h left` };
-
-  if (hours >= 3) data.color = "green";
-  else if (hours >= 2) data.color = "yellow";
-  else if (hours >= 1) data.color = "orange";
-  else {
-    data.timeLeft = "<30m left";
-    data.color = "red";
-  }
-
-  return data;
-};
->>>>>>> csb-e4kuf
